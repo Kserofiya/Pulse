442cf416382b343751a23e2d1eaeebf055c4f28c
Kseniya <ad50depo@yandex.ru>
2020-09-28 02:16:37 +0300

    finish sclider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right-arrow.png"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+              }
+            }
+        ]
+});
   });
