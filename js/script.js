6d438bb800082e2e05eb84d6da72dc3cde984463
Kseniya <ad50depo@yandex.ru>
2020-09-30 01:34:50 +0300

    finish carousel

--- js/script.js
+++ js/script.js
@@ -1,42 +1,15 @@
-// $(document).ready(function() {
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: false,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="img/left-arrow.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="img/right-arrow.png"></button>',
-//         responsive: [
-//             {
-//               breakpoint: 1024,
-//               settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 3,
-//                 infinite: true,
-//                 dots: true
-//               }
-//             },
-//             {
-//               breakpoint: 600,
-//               settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 2,
-//                 dots: true
-//               }
-//             },
-//             {
-//               breakpoint: 480,
-//               settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1,
-//                 dots: true
-//               }
-//             }
-//         ]
-// });
-//   });
-
-var slider = tns({
-  container: '.carousel',
+const slider = tns({
+  container: '.carousel__inner',
   items: 1,
   slideBy: 'page',
-  autoplay: true
+  autoplay: false,
+  controls: false,
+  nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
 });
