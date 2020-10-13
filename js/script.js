c3e7382b3241109e0751fcf09ba56d2c7092c56b
Kseniya <ad50depo@yandex.ru>
2020-10-13 03:23:26 +0300

    animation

--- js/script.js
+++ js/script.js
@@ -90,7 +90,7 @@ $('input[name=phone]').mask("+7 (999) 999-99-99");
 
 // Send email
 
-$('form').submit(function(e) {
+$('#order form').submit(function(e) {
   e.preventDefault();
   $.ajax({
     type: "POST",
@@ -98,9 +98,30 @@ $('form').submit(function(e) {
     data: $(this).serialize()
   }).done(function() {
       $(this).find('input').val("");
-
-
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn(slow);
       $(form).trigger('reset');
   });
   return false;
 });
+
+// Smooth scroll and pageup
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1600) {
+    $('.pageup').fadeIn();
+  } else {
+    $('.pageup').fadeOut();
+  }
+
+  $("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+});
+
+// animation
+
+new WOW().init();
+
--- mailer/smart.php
+++ mailer/smart.php
@@ -11,15 +11,15 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
+$mail->Host = 'ssl://smtp.gmail.com';;  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'ad50depo@yandex.ru';                 // Наш логин
-$mail->Password = 'demyan';                           // Наш пароль от ящика
+$mail->Username = 'testdemchenko@gmail.com';                 // Наш логин
+$mail->Password = 'kserokairan';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('ad50depo@yandex.ru', 'Pulse');   // От кого письмо 
-$mail->addAddress('kd50depo@yandex.ru');     // Add a recipient
+$mail->setFrom('testdemchenko@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('ad50depo@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
